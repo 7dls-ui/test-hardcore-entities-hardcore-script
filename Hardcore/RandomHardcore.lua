69f73807e10207964f7394e232643bf89e5770d3
Teo <teo.ploscaru@gmail.com>
2026-04-29 15:41:55 +0300

    Commit

---
+++ Hardcore/RandomHardcore.lua
@@ -0,0 +1,21 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+--[[
+   MADE BY
+
+     _____          _____  _      _____  __ ______
+    |  __ \   /\   |  __ \| |    |_   _|/_ |____  |
+    | |  | | /  \  | |__) | |      | |   | |   / /
+    | |  | |/ /\ \ |  _  /| |      | |   | |  / /
+    | |__| / ____ \| | \ \| |____ _| |_  | | / /
+    |_____/_/    \_\_|  \_\______|_____| |_|/_/
+                                     ______
+                                    |______|
+--]]
+--[[
+    or
+    https://github.com/Darli17/Entities/blob/main/Hardcore/HardcoreForOldHotel
+--]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Darli17/Entities/refs/heads/main/Hardcore/HardcoreForOldHotel"))()
